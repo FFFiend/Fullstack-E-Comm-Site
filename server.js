ff5f6eb31a88eccce7759f966f9ab30404c90a3c
FFFiend <owais.zahid@mail.utoronto.ca>
2023-01-08 05:42:55 -0500

    added changes to server.js file

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.sendFile('homepage.html');
+app.get('*', (req, res) => {
+    //res.sendFile('homepage.html');
+    res.send('Hi');
     // i believe this is listening to the homepage 
+
+    // do more homework on this. I don't fully understand how
+    // backend dev works.
 });
 
 app.listen(3000, () => {
